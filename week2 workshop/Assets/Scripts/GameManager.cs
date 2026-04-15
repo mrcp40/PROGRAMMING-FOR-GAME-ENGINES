c5dd0bd272e5be7c9aaa3fc8a163f9a63c197a47
mrcp40 <1440228025@qq.com>
2026-04-15 12:58:52 +0800

    week2 workshop
    exercise for pachinko game

---
+++ week2 workshop/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _scoreText = null;
+
+    private int _currentScore = 0;
+
+    private static GameManager instance=null;
+    public static GameManager Instance {  get { return instance; }  }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {  
+            instance = this; 
+            UpdateScore();
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddScore(int amount)
+    {
+        _currentScore += amount;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        _scoreText.text="Score: "+_currentScore.ToString();
+    }
+    
+}
