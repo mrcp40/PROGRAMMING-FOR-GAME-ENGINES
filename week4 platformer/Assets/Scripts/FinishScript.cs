7cb54d8f498b7a7ba86d231e75ced6e5c222fdde
mrcp40 <1440228025@qq.com>
2026-05-10 06:30:06 +0800

    Checkpoint
    input checkpoint logic(two player needed to win)
    fix animation bug

--- week4 platformer/Assets/Scripts/FinishScript.cs
+++ week4 platformer/Assets/Scripts/FinishScript.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class FinishScript : MonoBehaviour
 {
+    private bool _player1Complete = false;
+    private bool _player2Complete = false;
     private bool _levelComplete = false;
     void Start()
     {
@@ -11,10 +13,22 @@ public class FinishScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if ((collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")) && !_levelComplete)
+        if (collision.gameObject.CompareTag("Player1") && !_levelComplete)
+        {
+            _player1Complete = true;
+        }
+        if(collision.gameObject.CompareTag("Player2") && !_levelComplete)
+        {
+            _player2Complete = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (_player1Complete && _player2Complete && !_levelComplete)
         {
             _levelComplete = true;
-            Invoke("NextLevel", 2f);
+            NextLevel();
         }
     }
 
